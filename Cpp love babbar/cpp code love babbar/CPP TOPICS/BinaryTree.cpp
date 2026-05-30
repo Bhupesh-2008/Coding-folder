a31acaa4f1032894a01ee5288ef209e48a68fb83
Bhupesh-2008 <217087566+Bhupesh-2008@users.noreply.github.com.>
2026-05-30 15:31:59 +0530

    Binary tree

--- Cpp love babbar/cpp code love babbar/CPP TOPICS/BinaryTree.cpp
+++ Cpp love babbar/cpp code love babbar/CPP TOPICS/BinaryTree.cpp
@@ -32,9 +32,99 @@ Node* buildTree(Node* root){
     return root;
 }
 
+void LevelOrderTraversal(Node* root){
+    queue<Node*>q;
+    q.push(root);
+    q.push(NULL);
+
+    while(!q.empty()){
+        Node* temp= q.front();
+        q.pop();
+
+        if(temp==NULL){
+            cout<<"\n";
+            if(!q.empty()){
+                q.push(NULL);
+            }
+        }
+        else{
+            cout<<temp->data<<" ";
+            if(temp->left){
+                q.push(temp->left);
+            }
+
+            if(temp->right){
+                q.push(temp->right);
+        }
+        }
+    }
+}
+
+void inorder(Node* root){
+    if(root==NULL) return;
+    inorder(root->left);
+    cout<<root->data<<" ";
+    inorder(root->right);
+}
+
+void preorder(Node* root){
+    if(root==NULL)return;
+    cout<<root->data<<" ";
+    preorder(root->left);
+    preorder(root->right);
+}
+
+void postorder(Node* root){
+    if(root==NULL) return;
+    postorder(root->left);
+    postorder(root->right);
+    cout<<root->data<<" ";
+}
+
+void buildFromLevelOrderTraversal(Node* & root){
+    queue<Node*>q;
+    cout<<"Enter the data for root: ";
+    int data;
+    cin>>data;
+    root= new Node(data);
+    q.push(root);
+
+    while(!q.empty()){
+        Node* temp= q.front();
+        q.pop();
+
+        cout<<"Enter the data for left of "<<temp->data<<": ";
+        int leftData;
+        cin>>leftData;
+        if(leftData!=-1) {
+            temp->left= new Node(leftData);
+            q.push(temp->left);
+        }
+
+        cout<<"Enter the data for right of "<<temp->data<<": ";
+        int rightData;
+        cin>>rightData;
+        if(rightData!=-1) {
+            temp->right= new Node(rightData);
+            q.push(temp->right);
+        }
+    }
+}
 int main(){
     Node* root= NULL ;
-    root=buildTree(root);
+    // root=buildTree(root);
+    // // 1 3 7 -1 -1 11 -1 -1 5 17 -1 -1 -1 
+
+    // cout<<"\n Printing the data in the tree: ";
+    // LevelOrderTraversal(root);
+    // inorder(root);
+    // cout<<"\n";
+    // preorder(root);
+    // cout<<"\n";
+    // postorder(root);
+
+    buildFromLevelOrderTraversal(root);
+    LevelOrderTraversal(root);
 
     return 0;
 }
